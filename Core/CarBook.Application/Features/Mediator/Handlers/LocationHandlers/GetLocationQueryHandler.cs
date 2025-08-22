e00d6a1c5539664c4e398dd8edff59a24cc04960
mustafaarsln2004-pixel <mustafaarsln2004@gmail.com>
2025-08-22 09:34:42 +0300

    Udemy 37. Videoya Kadar tamamlandı.

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Carbook.Domain.Entities;
+using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using CarBook.Application.Features.Mediator.Results.FeatureResult;
+using CarBook.Application.Features.Mediator.Results.LocationResults;
+using CarBook.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.LocationHandlers
+{
+    public class GetLocationQueryHandler : IRequestHandler<GetLocationQuery, List<GetLocationQueryResults>>
+    {
+        private readonly IRepository<Location> _repository;
+
+        public GetLocationQueryHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetLocationQueryResults>> Handle(GetLocationQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+
+            return values.Select(x => new GetLocationQueryResults
+
+            {
+            LocationId = x.LocationId,
+            Name=x.Name
+
+
+            }).ToList();
+        }
+    }
+}
