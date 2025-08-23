9c0346719bacc57f6fdd0363fcb1942af091f16f
mustafaarsln2004-pixel <mustafaarsln2004@gmail.com>
2025-08-23 16:34:03 +0300

    Udemy CarBook Projesi 63. Videoya Kadar Tamamlandı.

---
+++ Frontends/CarBook.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using CarBook.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        
+
+        public async Task<IActionResult>Index()
+        {
+            
+            return View();
+        }
+    }
+}
--- Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
 {
     public class _TestimonialComponentPartial : ViewComponent
-    {private readonly IHttpClientFactory _httpClientFactory;
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
 
         public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
         {
@@ -14,12 +16,12 @@ namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client= _httpClientFactory.CreateClient();
-            var ResponseMessage = await client.GetAsync("https://localhost:7081/api/Testimonials");
+            var responseMessage = await client.GetAsync("https://localhost:7081/api/Testimonials");
             
-            if (ResponseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
-                var jsonData= await ResponseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
                 return View(values);
             }return View();
         }
